9d7e965e2b84b71e67553daedee4965b686541c7
Varitaxxx <post@asgardschmiede.eu>
2023-03-10 13:06:50 +0100

    Initial commit

---
+++ src/main/java/eu/asgardschmiede/productmanagement/ProductRepository.java
@@ -0,0 +1,75 @@
+package eu.asgardschmiede.productmanagement;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ProductRepository {
+
+    // Name der PersistenceUnit aus der persistence.xml
+    // Factory Konfiguriert die Erzeugung der EntityManager
+    public static EntityManagerFactory factory = Persistence.createEntityManagerFactory("default");
+
+    public static EntityManager getEntityManager() {
+        return factory.createEntityManager();
+    }
+
+    public void save(Product product) {
+
+        if(product.getId() > 0L) {
+            // UPDATE
+        }
+        else {
+            insert(product);
+        }
+    }
+
+    private void insert(Product product) {
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin(); // Transaktion starten
+        em.persist(product);
+        em.getTransaction().commit(); // Alle Befehle der Transaktion an die Datenbank übertragen
+    }
+
+    public Optional<Product> find(long id) {
+        EntityManager em = getEntityManager();
+        Product product = em.find(Product.class, id);
+        if(product != null) {
+            em.detach(product);
+            return Optional.of(product);
+        }
+        return Optional.empty();
+    }
+
+    public List<Product> find() {
+        EntityManager em = getEntityManager();
+        // JPQL = Java Persistance Query Language. Konzentriert sich auf Java-Klassen nicht auf Tabellen in der DB
+        List<Product> products = em.createQuery("SELECT p FROM Product p").getResultList();
+        return products;
+    }
+
+    public List<Product> find(String str) {
+        EntityManager em = getEntityManager();
+        // OR = Wert kann in name oder description vorkommen
+        // LIKE = Der Wert kann anders beginnen oder anders enden
+        // ?1 = Parameter, muss anschließend über setParameter zugewiesen werden
+        List<Product> products = em.createQuery("SELECT p FROM Product p WHERE p.name LIKE ?1 OR p.description LIKE ?1")
+                .setParameter(1, "%" + str + "%") // % = Beliebige Zeichen
+                .getResultList();
+        return products;
+    }
+
+    public void delete(long id) {
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        Product product = em.find(Product.class, id);
+        em.remove(product);
+        em.getTransaction().commit();
+    }
+
+    public void delete(Product product) {
+        delete(product.getId());
+    }
+}
---
+++ src/main/java/eu/asgardschmiede/productmanagement/ProductServlet.java
@@ -0,0 +1,48 @@
+package eu.asgardschmiede.productmanagement;
+import java.io.*;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(value = {"", "/products"})
+public class ProductServlet extends HttpServlet {
+
+    private ProductRepository repository = new ProductRepository();
+
+
+    // Direkter aufruf der URL (/products) mit GET. Übersicht der Produkte
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        // Die aufgerufenen URL entscheiden darüber, was das Servlet macht
+
+        // Parameter aus der URL lesen
+        final String action = request.getParameter("a");
+
+        if(action != null && action.equals("form")) { // Formular
+            getServletContext().getRequestDispatcher("/WEB-INF/form.jsp").forward(request, response);
+        }
+        else { // Auflistung der Produkte
+            final String search = request.getParameter("s"); // Einlesen von Suchparametern
+            if(search != null) {
+                request.setAttribute("products", repository.find(search));
+            }
+            else {
+                request.setAttribute("products", repository.find());
+            }
+            getServletContext().getRequestDispatcher("/WEB-INF/list.jsp").forward(request, response);
+        }
+    }
+
+    // Verarbeitung der Formulardaten (POST)
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        final String name = request.getParameter("name");
+        final String description = request.getParameter("description");
+        final double price = Double.parseDouble(request.getParameter("price"));
+
+        repository.save(new Product(name, description, price)); // Speichern
+
+        response.sendRedirect("products"); // Auf die Übersicht umleiten
+    }
+}
